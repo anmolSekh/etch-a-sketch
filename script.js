c2a943149e9dc460be44aa2b50844cf9a3fcc222
Anmol <anmol1314@hotmail.com>
2025-07-17 05:03:32 -0700

    grid is flexing to width, but not to height

--- script.js
+++ script.js
@@ -4,11 +4,12 @@
 
 //get it to work on 2x2 then scale to 16x16
 const content = document.querySelector("#content");
-
-for(i = 0; i < 100; i++) {
+let n = 16;
+for(i = 0; i < n; i++) {
     let div = document.createElement("div");
     div.classList.add("row");
-    for(j = 0; j < 100; j++) {
+    // div.classList.add("flex");
+    for(j = 0; j < n; j++) {
         let div2 = document.createElement("div");
         div2.classList.add("box");
         div2.addEventListener("mouseover", function(e) {
